fc91d8eaff1f25b08aefab0c5c87fcc5acc7926d
danielliu1973 <daniel.liu1973@gmail.com>
2016-06-08 09:08:16 -0400

    Add refactoring step

--- src/mytest/RBC.java
+++ src/mytest/RBC.java
@@ -4,7 +4,7 @@ public class RBC {
 	public int AMethod(boolean first, boolean second, boolean third)
     {
         int OptionOne = 1, OptionTwo = 2;
-        
+//Step 0: Initial version        
 //		if (!first)
 //        {
 //            if (second)
@@ -43,8 +43,163 @@ public class RBC {
 //            return OptionTwo;			// case 7: true true false
 //        }
 //        return OptionOne;				// case 8: true true true
+
+//Step 1: Delete invalid case 2       
+//		if (!first)
+//      {
+//          if (second)
+//          {
+//              if (third)
+//              {
+//                  return OptionOne;	// case	1: false true true
+//              }
+//              else
+//              {
+//                  return OptionOne;	// case 3: false true false
+//              }
+//          }
+//          else
+//          {
+//              return OptionOne;		// case 4: false false true || false false false
+//          }
+//      }
+//      else if (!third)
+//      {
+//          if (!second)
+//          {
+//              return OptionTwo;		// case 5: true false false
+//          }
+//      }
+//      else if (!second)
+//      {
+//          return OptionTwo;			// case 6: true false true
+//      }
+//      if (!(third || !second))
+//      {
+//          return OptionTwo;			// case 7: true true false
+//      }
+//      return OptionOne;				// case 8: true true true
+
+//Step 2: Combine case 1 and case 3        
+//		if (!first)
+//      {
+//          if (second)
+//          {  
+//             return OptionOne;	// case	1: false true true
+//          }
+//          else
+//          {
+//              return OptionOne;		// case 4: false false true || false false false
+//          }
+//      }
+//      else if (!third)
+//      {
+//          if (!second)
+//          {
+//              return OptionTwo;		// case 5: true false false
+//          }
+//      }
+//      else if (!second)
+//      {
+//          return OptionTwo;			// case 6: true false true
+//      }
+//      if (!(third || !second))
+//      {
+//          return OptionTwo;			// case 7: true true false
+//      }
+//      return OptionOne;				// case 8: true true true        
+ 
+//Step 3: Combine case 1 and case 4        
+//		if (!first)
+//      {  
+//             return OptionOne;	// case	1: false true true
+//      }
+//      else if (!third)
+//      {
+//          if (!second)
+//          {
+//              return OptionTwo;		// case 5: true false false
+//          }
+//      }
+//      else if (!second)
+//      {
+//          return OptionTwo;			// case 6: true false true
+//      }
+//      if (!(third || !second))
+//      {
+//          return OptionTwo;			// case 7: true true false
+//      }
+//      return OptionOne;				// case 8: true true true
         
+//Step 4: Reduce        
+//		if (!first)
+//		{
+//			return OptionOne;	// case	1: false true true
+//		}
+//		
+//		if (!third && !second)
+//		{
+//			return OptionTwo;		// case 5: true false false
+//		}
+//		
+//		if (!second)
+//		{
+//			return OptionTwo;			// case 6: true false true
+//		}
+//		
+//		if (!third && second)
+//		{
+//			return OptionTwo;			// case 7: true true false
+//		}
+//		
+//		return OptionOne;				// case 8: true true true      
+
+//Step 5: Combine case 5, case 6 and case 7        
+//		if (!first)
+//		{
+//			return OptionOne;	// case	1: false true true
+//		}
+//		
+//		if (!third && !second || !second || !third && second)
+//		{
+//			return OptionTwo;			// case 7: true true false
+//		}
+//		
+//		return OptionOne;				// case 8: true true true    
+		
+//Step 6: Simplify case 7        
+//		if (!first)
+//		{
+//			return OptionOne;	// case	1: false true true
+//		}
+//		
+//		if (!third || !second)
+//		{
+//			return OptionTwo;			// case 7: true true false
+//		}
         
+//Step 7:  rewrite case 7
+//		if (first &&(!third || !second))
+//		{
+//			return OptionTwo;			// case 7: true true false
+//		}
+//		
+//		if (!first)
+//		{
+//			return OptionOne;	// case	1: false true true
+//		}
+//		return OptionOne;				// case 8: true true true
+		
+//Step 8:  reduce
+//		if (first &&(!third || !second))
+//		{
+//			return OptionTwo;			// case 7: true true false
+//		}else
+//		{
+//			return OptionOne;	// case	1: false true true
+//		}
+		
+//Step 9:  Simplify				
         return first && !third || first && !second ? OptionTwo : OptionOne;				
         
     }
